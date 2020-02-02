c990c4294b022120a4c8639e0263d5e5a91c824c
ndts <nicolastorres@mailbox.org>
2020-02-02 23:03:54 +0100

    optimised attractive_numbers

--- attractive_numbers.fsx
+++ attractive_numbers.fsx
@@ -1,36 +1,37 @@
 // attractive_numbers.fsx
-
-// taken from Prime Decomposition
-let decompose_prime n =
-    let rec loop c p =
-        if c < (p * p) then [c]
-        elif c % p = 0 then p :: (loop (c / p) p)
-        else loop c (p + 1)
-    loop n 2
-
 // taken from Primality by trial division
-let rec primes = Seq.cache(Seq.append (seq[ 2; 3; 5 ]) (Seq.unfold (fun state -> Some(state, state + 2)) 7 |> Seq.filter (fun x -> is_prime x)))
+let rec primes =
+    let next_state s = Some(s, s + 2)
+    Seq.cache
+        (Seq.append
+            (seq[ 2; 3; 5 ])
+            (Seq.unfold next_state 7
+            |> Seq.filter is_prime))
 and is_prime number =
     let rec is_prime_core number current limit =
-        let cprime = primes |> Seq.nth current
-        if cprime >= limit then
-            true
-        else if number % cprime = 0 then
-            false
-        else
-            is_prime_core number (current + 1) (number/cprime)
- 
-    if number = 2 then
-        true
-    else if number < 2 then
-        false
-    else
-        is_prime_core number 0 number
+        let cprime = primes |> Seq.item current
+        if cprime >= limit then true
+        elif number % cprime = 0 then false
+        else is_prime_core number (current + 1) (number/cprime)
+    if number = 2 then true
+    elif number < 2 then false
+    else is_prime_core number 0 number
 
-// for each number decide
-// map to list of prime divisors
-// map to length
-// map to is_prime
+// taken from Prime decomposition task and modified to add
+let count_prime_divisors n =
+    let rec loop c n count =
+        let p = Seq.item n primes
+        if c < (p * p) then count
+        elif c % p = 0 then loop (c / p) n (count + 1)
+        else loop c (n + 1) count
+    loop n 0 1
 
-let is_attractive = decompose_prime >> List.length >> is_prime
-[1..120] |> List.filter is_attractive |> List.iter (printf "%d ")
+let is_attractive = count_prime_divisors >> is_prime
+let print_iter i n =
+    if i % 10 = 9
+    then printfn "%d" n
+    else printf "%d\t" n
+
+[1..120]
+|> List.filter is_attractive
+|> List.iteri print_iter
